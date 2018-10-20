8d78e72ee2f005b91fbcfc2aafdd7421c64899c7
Abhinav Kasamsetty <abhinavkasamsetty@gmail.com>
2018-10-20 14:55:51 -0500

    Added server file

---
+++ server/main.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
+# Transcribe audio file using rev.ai
+def transcribe_audio():
+    pass
+
+# Main method to test functions
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
